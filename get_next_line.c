c2d2ebf39a760742ea797ca5cda76349cd048b03
SANTOOS00 <mohamederraiss@icloud.com>
2025-11-07 14:40:36 +0100

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,12 @@
 /*   By: moerrais <moerrais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 05:09:13 by moerrais          #+#    #+#             */
-/*   Updated: 2025/11/07 13:56:13 by moerrais         ###   ########.fr       */
+/*   Updated: 2025/11/07 14:38:59 by moerrais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-int ft_strlen_custom(char *buffer, int tmp)
+int ft_len(char *buffer, int tmp)
 {
 	int i;
 
@@ -43,7 +43,7 @@ char *ft_extract_line(char *buffer)
 	int i = 0;
 	if (!buffer)
 		return NULL;
-	int size = ft_strlen_custom(buffer,0);
+	int size = ft_len(buffer,0);
 	if (*(buffer + size) == '\n')
 		size++;
 	char *res = malloc (sizeof (char) * (size + 1));
@@ -60,21 +60,21 @@ char *ft_extract_line(char *buffer)
 
 char *ft_trim_buffer(char *buffer)
 {
-	int size = ft_strlen_custom(buffer,0);
+	int size;
 	char *line;
 	int sizeline;
 	int i = 0;
 	if (!buffer)
 		return (NULL);
-	
-	if (*(buffer + size) == '\n')
+	size = ft_len(buffer,0);
+	if (buffer[size] == '\n')
 		size++;
 	else
 		return (free (buffer),NULL);	
-	sizeline = ft_strlen_custom(&buffer[size],1);
+	sizeline = ft_len(&buffer[size],1);
 	line = malloc (sizeof(char) * (sizeline + 1));
 	if (!line)
-		return (free (buffer),NULL);
+		return (free(buffer),NULL);
 	while (sizeline > i)
 	{
 		line[i] = buffer[size];
@@ -82,63 +82,48 @@ char *ft_trim_buffer(char *buffer)
 		size++;
 	}
 	line[i] = '\0';
-	free(buffer);
-	return (line);
+	return (free(buffer) ,line);
 }
-void ft_concat_strings(char *s1, char *s2, char *dest)
+
+char *ft_strjoin_custom(char *s1, char *s2)
 {
-	int sizes1 = ft_strlen_custom(s1,1);
-	int sizes2 = ft_strlen_custom(s2,1);
-	int i = 0;
-	int c = 0;
-	while ((sizes1 + sizes2) > i)
+	int i;
+	int j;
+	char *res;
+
+	res = malloc(ft_len(s1, 1) + ft_len(s2, 1) + 1);
+	if (!res)
+		return (free(s1), NULL);
+	i = 0;
+	j = 0;
+	while (s1 && s1[i])
 	{
-		if (sizes1 > i)
-		{
-			dest[i] = s1[i];
-			i++;
-		}
-		else
-			dest[i++] = s2[c++];
+		res[i] = s1[i];
+		i++;
 	}
-	dest[i] = '\0';
-	free(s1);
+	while (s2[j])
+		res[i++] = s2[j++];
+	res[i] = '\0';
+	return (free(s1),res);
 }
-char *ft_strjoin_custom(char *buffer, char *line)
-{
-	int sizebuffer = ft_strlen_custom(buffer,1);
-	int sizeline = ft_strlen_custom(line,1);
-	char *str;
-	str = malloc (sizeof(char) * (sizebuffer + sizeline + 1));
-	if (!str)
-		return (free (buffer),NULL);
-	ft_concat_strings(buffer,line,str);
-	return (str);
-}
-char *ft_read_until_newline(int fd ,char *buffer)
-{
-	char tmp[BUFFER_SIZE];
-	int bytes_read;
-	
-	while ((bytes_read = read (fd , tmp,BUFFER_SIZE)) > 0)
-	{
-		tmp[bytes_read] = '\0';
-		buffer = ft_strjoin_custom(buffer,tmp);
-		if (!buffer)
-			return (NULL);
-		if(ft_contains_char(buffer,'\n'))
-			return (buffer);
-	}
-	return (buffer);
-}
+
 char *get_next_line(int fd)
 {
 	static char *buffer;
-	char *tmp;
+	char tmp[BUFFER_SIZE];
+	int n;
+	char *buf;
 	
-	buffer = ft_read_until_newline(fd,buffer);
-	tmp = ft_extract_line(buffer);
-	buffer = ft_trim_buffer(buffer);	
-
-	return (tmp);
+	while ((n = read (fd , tmp,BUFFER_SIZE)) > 0)
+	{
+		tmp[n] = '\0';
+		buffer = ft_strjoin_custom(buffer,tmp);
+		if(ft_contains_char(buffer,'\n'))
+			break;
+	}
+	if (!buffer)
+		return NULL;
+	buf = ft_extract_line(buffer);
+	buffer = ft_trim_buffer(buffer);
+	return (buf);
 }
