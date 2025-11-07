f34fb6a5714ca40d3ec7c2aece60d4c1790c8114
Kh4wf0x <118027978+SANTOOS00@users.noreply.github.com>
2025-11-07 10:57:45 +0100

    ggg
    gg

--- get_next_line.c
+++ get_next_line.c
@@ -6,110 +6,169 @@
 /*   By: moerrais <moerrais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 05:09:13 by moerrais          #+#    #+#             */
-/*   Updated: 2025/11/07 02:04:55 by moerrais         ###   ########.fr       */
+/*   Updated: 2025/11/07 10:40:54 by moerrais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-int ft_strchr(char *str ,char sep)
+int ft_str_null_end_new(char *buffer)
 {
 	int i;
-	
 	i = 0;
-	while (str[i])
-	{
-		if (str[i] == sep)
-		{
-			return i;
-		}
-		i++;
-	}
-	return (0);
-}
-int ft_strlen(char *str)
-{
-	int i;
-	
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i])
+	while (buffer[i] && buffer[i] != '\n')
 	{
 		i++;
 	}
 	return (i);
 }
-char *ft_strjoin(char *s1, char *s2)
+int ft_strlen(char *buffer)
 {
-	char *buffer;
-	int sizes1;
-	int sizes2;
+	int i;
 
-	sizes1 = ft_strlen(s1);
-	sizes2 = ft_strlen(s2);
-	buffer = malloc (sizeof(char) * (sizes1 + sizes2 + 1));
+	i = 0;
+	if (!buffer)
+	{
+		return (0);
+	}
+	while (buffer[i])
+	{
+		i++;
+	}
+	return (i);
+}
+int ft_strchr(char *buffer, char sep)
+{
+	int i;
+	i = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == sep)
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+char *ft_strlcpy(char *buffer)
+{
+	int i = 0;
+	if (!buffer)
+	{
+		return NULL;
+	}
+	int size = ft_str_null_end_new(buffer);
+	if (*(buffer + size) == '\n')
+	{
+		size++;
+	}
+	char *res = malloc (sizeof (char) * (size + 1));
+	if (!res)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		res[i] = buffer[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char *ft_ta3dilline(char *buffer)
+{
 	if (!buffer)
 	{
 		return (NULL);
 	}
+	int size = ft_str_null_end_new(buffer);
+	char *line;
+	int sizeline;
 	int i = 0;
-	int ss = 0;
-	while (s1[i])
+	
+	if (*(buffer + size) == '\n')
+		size++;
+	else
 	{
-		buffer[i] = s1[i];
+		free(buffer);
+		return NULL;	
+	}
+	sizeline = ft_strlen(&buffer[size]);
+	line = malloc (sizeof(char) * (sizeline + 1));
+	if (!line)
+	{
+		free(buffer);
+		return NULL;
+	}
+	while (sizeline > i)
+	{
+		line[i] = buffer[size];
 		i++;
+		size++;
 	}
-	if (s2)
-	{
-		while (s2[ss])
-		{
-			buffer[i] = s2[ss];
-			i++;	
-			ss++;
-		}	
-	}
-	buffer[i] = '\0';
-	free (s2);
-	return (buffer);
+	line[i] = '\0';
+	free(buffer);
+	return (line);
 }
-char *ft_cheak_en_newlwn(int fd, char *buffer)
+void ft_strlcat(char *s1, char *s2, char *str)
 {
-	char line[BUFFER_SIZE];
-	while (read(fd,line, BUFFER_SIZE) > 0)	
+	int sizes1 = ft_strlen(s1);
+	int sizes2 = ft_strlen(s2);
+	int i = 0;
+	int c = 0;
+	while ((sizes1 + sizes2) > i)
 	{
-		buffer = ft_strjoin(line,buffer);
-		if(ft_strchr(line, '\n'))
+		if (sizes1 > i)
 		{
-			return (buffer);	
+			str[i] = s1[i];
+			i++;
+		}
+		else
+		{
+			str[i++] = s2[c++];
 		}
 	}
-	return (buffer);
+	str[i] = '\0';
+	free(s1);
 }
-char *ft_mun_line(char *buffer)
+char *ft_strjoin(char *buffer, char *line)
 {
-	int size;
-	char *tmp = buffer;
+	int sizebuffer = ft_strlen(buffer);
+	int sizeline = ft_strlen(line);
 	char *str;
-	int sizelen;
-
-	size = 0;
-	if (!(size = (ft_strchr(buffer , '\n'))))
+	str = malloc (sizeof(char) * (sizebuffer + sizeline + 1));
+	if (!str)
 	{
-		size = ft_strlen(buffer);
+		free (buffer);
+		return (NULL);
 	}
-	sizelen = ft_strlen (&buffer[size]);
+	ft_strlcat(buffer,line,str);
+	return (str);
+}
+char *ft_t3dil_and_read_line(int fd ,char *buffer)
+{
+	char line[BUFFER_SIZE];
 	
-	free(tmp);
+	while ((read(fd , line,BUFFER_SIZE)) > 0)
+	{
+		buffer = ft_strjoin(buffer,line);
+		if(ft_strchr(buffer,'\n'))
+		{
+			return (buffer);
+		}
+	}
 	return (buffer);
 }
 char *get_next_line(int fd)
 {
 	static char *buffer;
-	char tmp;
-	buffer = ft_cheak_en_newlwn(fd,buffer);
+	char *tmp;
 	
-	tmp = ft_mun_line(buffer);
+	buffer = ft_t3dil_and_read_line(fd,buffer);
+	tmp = ft_strlcpy(buffer);
+	buffer = ft_ta3dilline(buffer);	
+
 	return (tmp);
-	
 }
